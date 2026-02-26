33c3e245833a67803b3d90388c30e3cc0dd73143
Olga <xotaoshi@gmail.com>
2026-02-26 17:33:56 +0200

    Добавьте файлы проекта.

---
+++ Homework/Program.cs
@@ -0,0 +1,50 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
+
+/*
+ Создайте форму регистрации пользователя, которая использует следующие атрибуты для обеспечения достоверности и безопасности данных:
+
+1) [ValidateNever]: Исключает поле "TermsOfService" из проверки.
+2) [CreditCard]: Проверяет формат номера кредитной карты в поле "CreditCardNumber".
+3) [Compare]: Сравнивает пароли в полях "Password" и "ConfirmPassword".
+4) [EmailAddress]: Проверяет формат адреса электронной почты в поле "Email".
+5) [PhoneNumber]: Проверяет формат номера телефона в поле "PhoneNumber".
+6) [Range]: Ограничивает возраст пользователя в поле "Age" диапазоном от 18 до 100 лет.
+7) [RegularExpression]: Проверяет, содержит ли имя пользователя в поле "Username" только буквы, цифры и подчеркивания.
+8) [Required]: Делает поля "FirstName", "LastName", "Password", "ConfirmPassword" и "Email" обязательными.
+9) [StringLength]: Ограничивает длину имени пользователя (поле "Username") 20 символами, а пароля (поля "Password" и "ConfirmPassword") - 100 символами.
+10) [URL]: Проверяет формат URL-адреса в поле "Website".
+11) [Remote]: Проверяет уникальность имени пользователя ("Username") путем вызова метода действия на сервере.
+
+Создайте модель «RegisterViewModel» со свойствами, соответствующими перечисленным атрибутам. Добавьте атрибуты к соответствующим свойствам модели. 
+Создайте контроллер «AccountController» с методом «Register». В методе Register реализуйте логику проверки данных модели, используя атрибуты.
+В случае успешной проверки данных сохраните пользователя в базе данных.
+В случае ошибки отобразите сообщение об ошибке пользователю.
+ */
---
+++ Homework/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Homework.ViewModels
+{
+    /*
+     
+1) [ValidateNever]: Исключает поле "TermsOfService" из проверки.
+2) [CreditCard]: Проверяет формат номера кредитной карты в поле "CreditCardNumber".
+3) [Compare]: Сравнивает пароли в полях "Password" и "ConfirmPassword".
+4) [EmailAddress]: Проверяет формат адреса электронной почты в поле "Email".
+5) [PhoneNumber]: Проверяет формат номера телефона в поле "PhoneNumber".
+6) [Range]: Ограничивает возраст пользователя в поле "Age" диапазоном от 18 до 100 лет.
+7) [RegularExpression]: Проверяет, содержит ли имя пользователя в поле "Username" только буквы, цифры и подчеркивания.
+8) [Required]: Делает поля "FirstName", "LastName", "Password", "ConfirmPassword" и "Email" обязательными.
+9) [StringLength]: Ограничивает длину имени пользователя (поле "Username") 20 символами, а пароля (поля "Password" и "ConfirmPassword") - 100 символами.
+10) [URL]: Проверяет формат URL-адреса в поле "Website".
+11) [Remote]: Проверяет уникальность имени пользователя ("Username") путем вызова метода действия на сервере.
+     */
+    record class RegisterViewModel
+    {
+        [ValidateNever]
+        public string TempsOfService { get; set; }
+        
+        [CreditCard(ErrorMessage = "Please enter a valid credit card number.")]
+        public string CreditCard { get; set; }
+        
+        [Required(ErrorMessage = "You need to enter password")]
+        [StringLength(100, ErrorMessage = "Password must be between 6 and 100 characters.", MinimumLength = 6)]
+        public string Password { get; set; }
+        
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [Required(ErrorMessage = "You need to confirm password")]
+        [StringLength(100, ErrorMessage = "Password must be between 6 and 100 characters.", MinimumLength = 6)]
+        public string PasswordConfirm { get; set; }
+        
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [Required(ErrorMessage = "Email is required.")]
+        public MailAddress Email { get; set; }
+
+        [Phone(ErrorMessage = "Please enter a valid phone number.")]
+        public string PhoneNumber { get; set; }
+        
+        [Range(18, 100, ErrorMessage = "Age must be between 18 and 100.")]
+        public int Age { get; set; }
+        
+        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Username can only contain letters, numbers, and underscores.")]
+        [StringLength(20, ErrorMessage = "Username cannot be longer than 20 characters.")]
+        [Remote(action: "IsUsernameAvailable", controller: "Account", ErrorMessage = "Username is already taken.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+
+        [Url(ErrorMessage = "Please enter a valid URL.")]
+        public string Website { get; set; }
+    }
+}
